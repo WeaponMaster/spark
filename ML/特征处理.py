261b0e33aba04a796fed7aabdc8f75a0f5be8b95
xyls <1925730614@qq.com>
2022-04-27 00:23:10 +0800

    增加了机器学习模型训练部分

--- ML/特征处理.py
+++ ML/特征处理.py
@@ -3,7 +3,9 @@ import pyspark.sql.functions as fn
 from pyspark.ml.feature import StringIndexer, OneHotEncoderEstimator
 from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF
 from pyspark.ml.feature import VectorAssembler, StandardScaler
-
+from pyspark.sql.types import StructType,StructField,StringType,DoubleType
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 
 def test_string_indexer():
@@ -34,15 +36,42 @@ def test_stdscaler():
     scaled = StandardScaler(inputCol='axes',outputCol='axes_std_scaled',withMean=True).fit(vector).transform(vector)
     scaled.show()
 
-
+def test_ml():
+    # iris.describe().show()
+    # 转变variety的的类型
+    iris1 = StringIndexer(inputCol='variety',outputCol='label').fit(iris).transform(iris)
+    # iris1.show()
+    # 特征向量化
+    assembler = VectorAssembler(inputCols=["sepal_length",'sepal_width','petal_length','petal_width'],outputCol='features')
+    features = assembler.transform(iris1)
+    # features.show()
+    train,test = features.randomSplit([0.8,0.2],seed=42)
+    # train.show()
+    lr = LogisticRegression()
+    model = lr.fit(train)
+    predict = model.transform(test)
+    # predict.printSchema()
+    # predict.select('sepal_length','sepal_width','petal_length','petal_width','label','prediction').show()
+    evaluator = MulticlassClassificationEvaluator(metricName='f1')
+    print(evaluator.evaluate(predict))
 
 if __name__ == '__main__':
     spark = SparkSession.builder.master('local[4]').appName('feature').getOrCreate()
     df = spark.read.json('E:/spark/data/zipcodes.json')
+    schema = StructType([StructField('sepal_length',DoubleType(), True),
+                        StructField('sepal_width', DoubleType(), True),
+                        StructField('petal_length', DoubleType(), True),
+                        StructField('petal_width',DoubleType(), True),
+                        StructField('variety',StringType(),True)
+                        ])
+    iris = spark.read.csv(path='E:/spark/data/iris.csv',header=True, schema=schema)
+    # iris.show()
+    # iris.printSchema()
     # df.printSchema()
     # df.show()
     # test_string_indexer()
     # test_onehot()
     # test_nlp()
     # test_vector()
-    test_stdscaler()
+    # test_stdscaler()
+    test_ml()
